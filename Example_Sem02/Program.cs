39b07d6b39182c764b16c5a0defca46a860fbb42
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-11 20:36:54 +0300

    fix: finding the maximum of a two-digit number

---
+++ Example_Sem02/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая выводит случайное число 
+из отрезка [10, 99] и показывает наибольшую цифру числа.*/
+
+int value = new Random().Next(10,100);
+Console.WriteLine(value);
+
+int a = value / 10;
+int b = value % 10;
+Console.WriteLine("Наибольшее число:");
+if (a>b)
+    {
+        Console.WriteLine(a);
+    }
+else if (a < b)
+    {
+        Console.WriteLine(b);
+    }
+else
+    {
+        Console.WriteLine("Числа равны");
+    }

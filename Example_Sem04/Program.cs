57aeee41538412aa72272ac755375e2ec6533ddb
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-18 20:45:41 +0300

    fix: program count numbers

--- Example_Sem04/Program.cs
+++ Example_Sem04/Program.cs
@@ -3,20 +3,42 @@
 // 7 -> 28
 // 4 -> 10
 // 8 -> 36
+// Console.WriteLine("Введите число: ");
+// int value = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+// int count = 0;
+
+// int GetSum()
+// {
+//     while (value > count)
+//     {
+//         count++;
+//         sum += count;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine(GetSum());
+
+
+// Напишите программу, которая принимает на вход число и выдаёт 
+// количество цифр в числе.
+// // 456 -> 3
+// // 78 -> 2
+// // 89126 -> 5
 Console.WriteLine("Введите число: ");
 int value = Convert.ToInt32(Console.ReadLine());
+int count=0;
 
-int sum = 0;
-int count = 0;
-
-int GetSum()
+int GetNumCount()
 {
-    while (value > count)
-    {
-        count++;
-        sum += count;
-    }
-    return sum;
+    while(value != 0)
+        {
+            value /= 10;
+            count++;
+        }
+    return count;
 }
 
-Console.WriteLine(GetSum());
+Console.WriteLine(GetNumCount());

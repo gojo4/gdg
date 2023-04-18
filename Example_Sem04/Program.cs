42a037fc3e0eafba2e1be794fcd0ff92bdbe7c82
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-18 20:53:30 +0300

    fix: program factorial

--- Example_Sem04/Program.cs
+++ Example_Sem04/Program.cs
@@ -27,18 +27,32 @@
 // // 456 -> 3
 // // 78 -> 2
 // // 89126 -> 5
+// Console.WriteLine("Введите число: ");
+// int value = Convert.ToInt32(Console.ReadLine());
+// int count=0;
+
+// int GetNumCount()
+// {
+//     while(value != 0)
+//         {
+//             value /= 10;
+//             count++;
+//         }
+//     return count;
+// }
+
+// Console.WriteLine(GetNumCount());
+
+// Задача 28: Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
 Console.WriteLine("Введите число: ");
 int value = Convert.ToInt32(Console.ReadLine());
-int count=0;
+int temp = 1;
 
-int GetNumCount()
-{
-    while(value != 0)
-        {
-            value /= 10;
-            count++;
-        }
-    return count;
-}
-
-Console.WriteLine(GetNumCount());
+for (int i = 1; i <= value; i++)
+    {
+        temp *= i;
+    }
+Console.WriteLine(temp);
